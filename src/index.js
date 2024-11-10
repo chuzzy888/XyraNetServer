bfaaeb80d7af75a0b7068c740e59d9723f0e4dc7
Your Name <chizupraise31@gmail.com>
2024-11-10 12:41:41 +0100

    added live urls

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ const apiRoutes = require("./routes/api");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS configuration
+const corsOptions = {
+  origin: "https://xyra-net.vercel.app/", // replace with your frontend's live URL
+  methods: ["GET", "POST", "PUT", "DELETE"], // specify allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // specify allowed headers
+};
+
+// Apply the CORS middleware to the app
+app.use(cors(corsOptions));
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -16,5 +25,6 @@ app.use("/api", apiRoutes);
 
 // Start server
 app.listen(PORT, () => {
+  // console.log(`Server is running on http://localhost:${PORT}`);
   console.log(`Server is running on http://localhost:${PORT}`);
 });
