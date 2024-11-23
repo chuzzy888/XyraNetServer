f17fd032b64d4a6bf1e910c20f051848289769ab
Your Name <chizupraise31@gmail.com>
2024-11-23 13:26:24 +0100

    removed cors

--- src/index.js
+++ src/index.js
@@ -8,16 +8,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // CORS configuration
-const corsOptions = {
-  origin: ["https://xyra-net.vercel.app/", "http://localhost:5173/"],
-  methods: ["GET", "POST", "PUT", "DELETE"], // specify allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // specify allowed headers
-};
+// const corsOptions = {
+//   origin: ["https://xyra-net.vercel.app/", "http://localhost:5173/"],
+//   methods: ["GET", "POST", "PUT", "DELETE"], // specify allowed HTTP methods
+//   allowedHeaders: ["Content-Type", "Authorization"], // specify allowed headers
+// };
 
-// Apply the CORS middleware to the app
-app.use(cors(corsOptions));
+// // Apply the CORS middleware to the app
+// app.use(cors(corsOptions));
 // Middleware
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 // Routes
