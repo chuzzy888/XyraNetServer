f99fd8d0c1e9e911cd2f8cf0693589b95b6ed62f
Your Name <chizupraise31@gmail.com>
2024-11-23 03:50:52 +0100

    added cors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration
 const corsOptions = {
-  origin: "https://xyra-net.vercel.app/", // replace with your frontend's live URL
+  origin: ["https://xyra-net.vercel.app/", "http://localhost:5173/"],
   methods: ["GET", "POST", "PUT", "DELETE"], // specify allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // specify allowed headers
 };
@@ -17,7 +17,7 @@ const corsOptions = {
 // Apply the CORS middleware to the app
 app.use(cors(corsOptions));
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 // Routes
